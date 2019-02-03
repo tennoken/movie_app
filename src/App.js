0f6f6e84a23a876f6d4b13f166b866c3cc187caf
SwiftCloud <tennoken@naver.com>
2019-02-03 15:56:05 +0900

    Study Loading state

--- src/App.js
+++ src/App.js
@@ -6,47 +6,61 @@ import Movie from './Movie';
 
 
 
-const movies = [
-  {
-    
-    title : "Interstella",
-    poster : "https://decipherscifi.com/wp-content/uploads/sites/4/2016/05/Interstellar-2-1024x576.jpg"
-  },
-  {
-    
-    title : "Interstella",
-    poster : "https://decipherscifi.com/wp-content/uploads/sites/4/2016/05/Interstellar-2-1024x576.jpg"
-  },
-  {
-    
-    title : "Interstella",
-    poster : "https://decipherscifi.com/wp-content/uploads/sites/4/2016/05/Interstellar-2-1024x576.jpg"
-  },
-  {
-    
-    title : "Interstella",
-    poster : "https://decipherscifi.com/wp-content/uploads/sites/4/2016/05/Interstellar-2-1024x576.jpg"
-  }
-
-]
-
-
 class App extends Component {
 
   // Render: componentWillMount() -> render() -> componentDidMount()
   // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+
+  
+  state = {
+
+  }
+  
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        movies : [
+          {
+            title : "Interstella",
+            poster : "https://decipherscifi.com/wp-content/uploads/sites/4/2016/05/Interstellar-2-1024x576.jpg"
+          },
+          {
+            
+            title : "Interstella",
+            poster : "https://decipherscifi.com/wp-content/uploads/sites/4/2016/05/Interstellar-2-1024x576.jpg"
+          },
+          {
+            
+            title : "Interstella",
+            poster : "https://decipherscifi.com/wp-content/uploads/sites/4/2016/05/Interstellar-2-1024x576.jpg"
+          },
+          {
+            
+            title : "Interstella",
+            poster : "https://decipherscifi.com/wp-content/uploads/sites/4/2016/05/Interstellar-2-1024x576.jpg"
+          },
+          {
+            title : "Inception",
+            poster : "https://upload.wikimedia.org/wikipedia/en/thumb/1/18/Inception_OST.jpg/220px-Inception_OST.jpg"
+          }
+        
+        ]
+      })
+    },5000)
+  }
+
+
+  _renderMovie = () => {
+    const movies = this.state.movies.map((movie, index) =>{
+      return <Movie title={movie.title} poster={movie.poster} key={index}/>
+    })
+    return movies;
+  }
   
   render() {
     return (
       <div className="App">
-        {movies.map((movie, index) =>{
-          return <Movie title={movie.title} poster={movie.poster} key={index}/>
-        })}
-        
-
-        
-        
-      
+       {this.state.movies ? this._renderMovie() : 'Loading'}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Movie.css';
 
+    
 
 class Movie extends Component {
+
+    // 부모 컴퍼넌트에게서 받는 정보의 종류가 무엇인지 체크할 수 있다.
+    static propTypes = {
+        title : PropTypes.string.isRequired,
+        poster : PropTypes.string.isRequired
+        
+    }
+
     render() {
         
         return (
@@ -16,6 +26,10 @@ class Movie extends Component {
 
 
 class MoviePoster extends Component {
+
+    static propTypes = {
+        poster : PropTypes.string.isRequired
+    }
 
     render() {
         return (
