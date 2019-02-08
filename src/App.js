6e4060fc0fe5280710a01c3268306299ccb6c7ef
SwiftCloud <tennoken@naver.com>
2019-02-08 17:25:33 +0900

    Updating Movie Component

--- src/App.js
+++ src/App.js
@@ -21,33 +21,39 @@ class App extends Component {
   }
 
 
-  _renderMovie = () => {
+  _renderMovies = () => {
     const movies = this.state.movies.map((movie) =>{
-      console.log(movie)
-      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id}/>
-    })
+      return (<Movie 
+                title={movie.title_english} 
+                poster={movie.medium_cover_image} 
+                key={movie.id} 
+                genres={movie.genres} 
+                synopsis={movie.synopsis}
+              />
+      );
+    });
     return movies;
-  }
+  };
 
   _getMovies = async () => {
       const movies = await this._callApi();
       this.setState({ // _callApi()가 완료되고 실행됨(성공하든 실패하든)
         movies  // movies : movies
-      })
-      
-  }
+      }); 
+  };
 
   _callApi = () => {
     return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
     .then(response => response.json())
     .then(json => json.data.movies)
-    .catch(err => console.log(err))
-  }
+    .catch(err => console.log(err));
+  };
   
   render() {
+    const { movies } = this.state;
     return (
-      <div className="App">
-       {this.state.movies ? this._renderMovie() : 'Loading'}
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
