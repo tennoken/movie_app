c44402940893df9c9bfbe31c27691bac9463f521
tennoken <tennoken@naver.com>
2019-02-11 14:35:03 +0900

    Updates

---
+++ precache-manifest.fd1d4bb8280bc9569a5a28ca891d99e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "916a49df4cf792586187",
+    "url": "/tennoken/movie_app/static/js/runtime~main.916a49df.js"
+  },
+  {
+    "revision": "4f024d44d253f2c6c7aa",
+    "url": "/tennoken/movie_app/static/js/main.4f024d44.chunk.js"
+  },
+  {
+    "revision": "22fdec2062f68dfa3e16",
+    "url": "/tennoken/movie_app/static/js/1.22fdec20.chunk.js"
+  },
+  {
+    "revision": "4f024d44d253f2c6c7aa",
+    "url": "/tennoken/movie_app/static/css/main.d6cc45b1.chunk.css"
+  },
+  {
+    "revision": "77f89fce03775c6f288ce2c3acdf757d",
+    "url": "/tennoken/movie_app/index.html"
+  }
+];
